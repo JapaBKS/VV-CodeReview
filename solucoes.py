dcbd3669963a632ddfc2cd79b7a86641e083e43e
Amanda Fila <amandafiladelima@gmail.com>
2025-09-03 19:30:01 -0300

    Atualização com tratamento de exceção

--- solucoes.py
+++ solucoes.py
@@ -1,8 +1,11 @@
 def sao_anagramas(string1, string2):
-  palavra1, palavra2 = string1.replace(" ", ""), string2.replace(" ", "")
-  palavra1, palavra2 = palavra1.lower(), palavra2.lower()
-  list1, list2 = list(palavra1), list(palavra2)
-  return sorted(list1) == sorted(list2)
+  try:
+    palavra1, palavra2 = string1.replace(" ", ""), string2.replace(" ", "")
+    palavra1, palavra2 = palavra1.lower(), palavra2.lower()
+    list1, list2 = list(palavra1), list(palavra2)
+    return sorted(list1) == sorted(list2)
+  except:
+    print("Formato inválido")
 
 def cifra_de_cesar(texto, deslocamento):
   # TODO: Implementar a lógica
